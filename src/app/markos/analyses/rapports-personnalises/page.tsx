b0183d0cf4d1c83b1f35062326e79afdf1625a07
Ahmed Silué <silueahmed273@gmail.com>
2025-07-12 14:06:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/markos/analyses/rapports-personnalises/page.tsx
+++ src/app/markos/analyses/rapports-personnalises/page.tsx
@@ -12,6 +12,7 @@ import { fr } from 'date-fns/locale';
 import { Label } from '@/components/ui/label';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 import { Checkbox } from '@/components/ui/checkbox';
+import { Input } from '@/components/ui/input';
 
 type Report = {
     id: string;
